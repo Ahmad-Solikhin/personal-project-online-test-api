03ed6b5258b5df337cf41c928b844e4ffe599c66
Gayuh <ahmadsgr39@gmail.com>
2026-03-19 21:14:01 +0700

    Add home link return message

---
+++ src/main/java/com/gayuh/personalproject/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.gayuh.personalproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String home() {
+        return "Welcome to personal project online test api";
+    }
+}
